529f19e7a72a66aaca7971bdd0eae5911550ba77
Poitier Stringer <stringerpoitier@gmail.com>
2020-11-26 13:04:27 -0800

    added paypal sdk

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Payment from './components/Payment.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Payment></Payment>
+    </div>
+  );
+}
+
+export default App;
